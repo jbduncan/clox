a7ec719d2e37ab1f8f41e158c89f18af1fb28a4f
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2021-07-03 20:27:27 +0100

    Create initial version of clox
    This follows the instructions in Chapter 14 of the online book
    craftinginterpreters.com.

---
+++ memory.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "memory.h"
+
+void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
+    if (newSize == 0) {
+        free(pointer);
+        return NULL;
+    }
+
+    void* result = realloc(pointer, newSize);
+    if (result == NULL) {
+        // realloc failed to find any spare blocks of memory with the
+        // requested size, so there's nothing the VM can do other
+        // than exit as gracefully as possible.
+        exit(1);
+    }
+    return result;
+}
---
+++ memory.h
@@ -0,0 +1,28 @@
+#ifndef clox_memory_h
+#define clox_memory_h
+
+#include "common.h"
+
+#define GROW_CAPACITY(capacity) \
+    ((capacity) < 8 ? 8 : (capacity) * 2)
+
+#define GROW_ARRAY(type, pointer, oldCount, newCount) \
+    (type*)reallocate(pointer, sizeof(type) * (oldCount), \
+        sizeof(type) * (newCount))
+
+#define FREE_ARRAY(type, pointer, oldCount) \
+    reallocate(pointer, sizeof(type) * (oldCount), 0)
+
+/**
+ * Manages the memory pointed to by `pointer`, following these rules:
+ *
+ * oldSize  | newSize              | Operation
+ * =============================================================
+ * 0        | Non-zero             | Allocate new block.
+ * Non-zero | 0                    | Free allocation.
+ * Non-zero | Smaller than oldSize | Shrink existing allocation.
+ * Non-zero | Larger than oldSize  | Grow existing allocation.
+ */
+void* reallocate(void* pointer, size_t oldSize, size_t newSize);
+
+#endif //clox_memory_h
